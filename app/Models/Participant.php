ac99c87dc15cb18045dcd58f476700c45012b78b
Guilherme Dantas <guilhermednts2@gmail.com>
2026-01-13 19:02:45 -0300

    fix/ ajustando o id o participant no model

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Participant extends Model
 {
+    protected $primaryKey = 'participant_id';
+
     protected $fillable = [
         'name',
         'badge',
